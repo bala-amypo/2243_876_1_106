910f6a95bc8589641753a555175de2f07d1997ae
bala-amypo <bala@amypo.in>
2025-12-18 04:05:33 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/Sensor.java
@@ -0,0 +1,30 @@
+package com.example.demo.entity;
+
+import jarkata.persistence.*;
+
+@Entity
+
+public class Sensor {
+
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+
+
+@Column(unique = true)
+private String sensorCode;
+
+
+private String sensorType;
+
+
+@ManyToOne
+private Location location;
+
+
+private LocalDateTime installedAt = LocalDateTime.now();
+
+
+private Boolean isActive = true;
+}
