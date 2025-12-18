02ad6fda3b77762124c5856a2bdd2c457914f0a1
bala-amypo <bala@amypo.in>
2025-12-18 16:19:09 +0000

    User commit

--- src/main/java/com/example/demo/repository/ComplianceLogRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.ComplianceLog;
-
-public interface ComplianceLogRepository extends JpaRepository<ComplianceLog, Long> {
-    ComplianceLog findBySensorReading_Id(Long id);
-}
--- src/main/java/com/example/demo/repository/ComplianceThresholdRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.ComplianceThreshold;
-
-public interface ComplianceThresholdRepository extends JpaRepository<ComplianceThreshold, Long> {
-    ComplianceThreshold findBySensorType(String type);
-}
